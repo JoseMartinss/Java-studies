c99e319f581fbe433ab11c12fef9c6ed02c66796
José Martins <jose.p.martins31@gmail.com>
2026-06-19 14:06:09 -0300

    Treino conversor texto numero
    Aprendi melhor:
    a usar os wrapplers, var primitivas e a dar printf

---
+++ Conversor texto-numero.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String textoPreco1 = "25.50";
+        String textoPreco2 = "14.50";
+
+      double p1 = Double.parseDouble(textoPreco1);
+      double p2 = Double.parseDouble(textoPreco2);
+
+      double resultado;
+      resultado = p1+p2;
+
+      System.out.println("O resultado é: " + resultado);
+
+
+        }
+    }
