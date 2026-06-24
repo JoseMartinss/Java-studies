dd7a29734862db906a3314c85e08aa6b7f889f69
José Martins <jose.p.martins31@gmail.com>
2026-06-24 11:42:08 -0300

    Create TreinoArray.java

---
+++ TreinoArray.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int[] ultimasSenhas = {101, 102, 103, 104};
+
+        ultimasSenhas[0] = 199;
+
+        System.out.println("Nova Primeira senha: "+ ultimasSenhas[0]);
+
+    }
+}
